39cbbf941f49fd76dda70d20b1556d439cafab7e
towhid135 <52478209+towhid135@users.noreply.github.com>
2019-07-06 02:39:04 +0600

    signup_model

--- library/projectdatabase/models.py
+++ library/projectdatabase/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
-class  logintable(models.Model):
-    role = models.CharField(max_length=15)
-    user = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
+class  signuptable(models.Model):
+    user = models.CharField(max_length=200,default="null")
+    password = models.CharField(max_length=200,default="null")
+    name = models.CharField(max_length=200,default="null")
+    department=models.CharField(max_length=200,default="null")
+    ID = models.CharField(max_length=200,primary_key=True,default="null")
+    sequrity = models.CharField(max_length=200,default="null")
+    answer = models.CharField(max_length=200,default="null")
+    role = models.CharField(max_length=15,default="null")
+
+    def __str__(self):
+        return self.name + "-" + self.ID
